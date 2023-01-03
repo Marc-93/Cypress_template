fa659f9093b44fa2c230eff8b1ed15319b47be8a
Marc_AC <marc93ac@gmail.com>
2023-01-03 19:38:44 +0100

    adding steps

--- cypress/features/test.js
+++ cypress/features/test.js
@@ -1,8 +1,18 @@
+/// <reference types="Cypress"  />  
+  
+const web_url = 'https://rahulshettyacademy.com/seleniumPractise/#/'
+const search_bar = '.search-keyword'
+const product_list = '.product'
+
+
   describe('Test suite', ()=> 
   {
     it('test case', ()=>
     {
-        cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
+        cy.visit(web_url)
+        cy.get(search_bar).type("cucumber")
+        cy.wait(1000)
+        cy.get(product_list).should('have.lengt',1)
     })
 
     it('test case', ()=>
