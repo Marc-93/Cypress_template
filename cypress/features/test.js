0d5265ebbbe659656d022700e3cbd05f98ae504a
Marc_AC <marc93ac@gmail.com>
2023-01-05 01:41:31 +0100

    update tests

--- cypress/features/test.js
+++ cypress/features/test.js
@@ -1,8 +1,13 @@
 /// <reference types="Cypress"  />  
+
+
   
 const web_url = 'https://rahulshettyacademy.com/seleniumPractise/#/'
 const search_bar = '.search-keyword'
-const product_list = '.product'
+const product_list =  '.products'
+const product = '.product:visible'
+const add_to_cart = '.product-action button'
+
 
 
   describe('Test suite', ()=> 
@@ -10,13 +15,15 @@ const product_list = '.product'
     it('test case', ()=>
     {
         cy.visit(web_url)
-        cy.get(search_bar).type("cucumber")
-        cy.wait(1000)
-        cy.get(product_list).should('have.lengt',1)
-    })
-
-    it('test case', ()=>
-    {
-        expect(true).to.equal(true)
+        cy.get(search_bar).type("cu")
+        cy.get(product_list).find(product).should('have.length.at.least',1)
+        cy.get(product).eq(0).find(add_to_cart).click()
+        // using contains cy.get(product).eq(1).contains('ADD TO CART').click()
+        cy.get(product_list).find(product).each(($el, index, $list) => {
+          const product_text = $el.find('h4.product-name').text().toLocaleLowerCase()
+          if(product_text.includes("capsicum")){
+            cy.wrap($el).find(add_to_cart).click()
+          }
+        })
     })
   })
