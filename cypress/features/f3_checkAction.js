d3d1fc9b47f6ba063fdfde9b1db204d36296e6d9
Marc_AC <marc93ac@gmail.com>
2023-01-11 18:10:54 +0100

    renaming check actions

--- cypress/features/f3_checkAction.js
+++ cypress/features/f3_checkAction.js
@@ -4,17 +4,26 @@
 
 const web_url = 'https://rahulshettyacademy.com/AutomationPractice/#/'
 const checkbox_1 = '#checkBoxOption1'
+const radio_1 = "[value='radio1']"
+const radio_2 = "[value='radio2']"
+const radio_3 = "[value='radio3']"
 
 
   describe('Test suite 3', ()=>
   {
-    it('test 1: Checkbox', ()=>
+      it('test 1: Checkbox', ()=>
     {
        // Opens the url
       cy.visit(web_url)
       cy.get(checkbox_1).check().should('be.checked').and('have.value', 'option1')
       cy.get(checkbox_1).uncheck().should('not.be.checked')
       cy.get("input[type='checkbox']").check(['option2', 'option3'])
+    })
 
+      it('test 2: Radiobuttons', ()=>
+    {
+        // Opens the url
+        cy.visit(web_url)
+        cy.get(radio_1).check().should('be.checked')
     })
   })
