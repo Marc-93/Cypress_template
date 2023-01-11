642da6aca537edcb617ecddc7530d1a9463427cc
Marc_AC <marc93ac@gmail.com>
2023-01-11 14:09:12 +0100

    refactoring tests + adding dropdown tc's

--- cypress/features/f3_checkbox.js
+++ cypress/features/f3_checkbox.js
@@ -1,14 +1,14 @@
-/// <reference types="Cypress"  />  
+/// <reference types="Cypress"  />
 
 
-  
+
 const web_url = 'https://rahulshettyacademy.com/AutomationPractice/#/'
 const checkbox_1 = '#checkBoxOption1'
 
 
-  describe('Test suite 3', ()=> 
+  describe('Test suite 3', ()=>
   {
-    it('test 1: Interact with elements', ()=>
+    it('test 1: Checkbox', ()=>
     {
        // Opens the url
       cy.visit(web_url)
---
+++ cypress/features/f4_dropdowns.js
@@ -0,0 +1,32 @@
+/// <reference types="Cypress"  />
+
+
+const web_url = 'https://rahulshettyacademy.com/AutomationPractice/#/'
+const dropdown_static = '#dropdown-class-example'
+const dropdown_values = ['option1', 'option2', 'option3']
+const dropdown_dynamic = '#autocomplete'
+
+
+  describe('Test suite 3', ()=>
+  {
+      it('test 1: Static dropdown', ()=>
+    {
+        // Opens the url
+        cy.visit(web_url)
+        cy.get(dropdown_static).select(dropdown_values[0]).should('have.value', dropdown_values[0]).log(dropdown_values[0])
+    })
+      it('test 2: Dynamic dropdown', ()=>
+      {
+          // Opens the url
+          cy.visit(web_url)
+          cy.get(dropdown_dynamic).type('ind')
+          cy.get('.ui-menu-item div').each(($element, index, $el_list) =>
+          {
+              if($element.text().toLowerCase() === 'india')
+                  {
+                      $element.click()
+                  }
+          })
+          cy.get('#autocomplete').should('have.value', 'India')
+      })
+  })
