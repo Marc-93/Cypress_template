290f4c0ea7a237ad889697ef20510a2c84eeb41f
Marc <marc.a@bling.eu>
2023-01-11 13:25:48 +0100

    add feature

---
+++ cypress/features/feature_3.js
@@ -0,0 +1,20 @@
+/// <reference types="Cypress"  />  
+
+
+  
+const web_url = 'https://rahulshettyacademy.com/AutomationPractice/#/'
+const checkbox_1 = '#checkBoxOption1'
+
+
+  describe('Test suite 3', ()=> 
+  {
+    it('test 1: Interact with elements', ()=>
+    {
+       // Opens the url
+      cy.visit(web_url)
+      cy.get(checkbox_1).check().should('be.checked').and('have.value', 'option1')
+      cy.get(checkbox_1).uncheck().should('not.be.checked')
+      cy.get("input[type='checkbox']").check(['option2', 'option3'])
+
+    })
+  })
