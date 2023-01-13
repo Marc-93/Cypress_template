c273675fc2bd16c969049ab061e0af6b290b8e82
Marc_AC <marc93ac@gmail.com>
2023-01-13 12:43:21 +0100

    Adding tables example

---
+++ cypress/features/f8_tables.js
@@ -0,0 +1,28 @@
+/// <reference types="Cypress"  />
+
+const web_url = 'https://rahulshettyacademy.com/AutomationPractice/#/'
+const table = "table[class='table-display']"
+const second_column = "tr td:nth-child(2)"
+
+describe('Test suite 5', ()=>
+{
+  it('test 8: tables', ()=>
+{
+    // Opens the url
+    cy.visit(web_url)
+    cy.get(table).should('be.visible')
+    cy.get(`${table} ${second_column}`).each(($element, index, $list) => {
+        const current_text = $element.text().toLowerCase()
+        if(current_text.includes('python')){
+            // here we need to check the price of this cours, but to use next sibling, we need to use it directly on get
+            // what we can do is use same command but recovering the index from each function
+            cy.get(`${table} ${second_column}`).eq(index).next().should('have.text', '25')
+            cy.get(`${table} ${second_column}`).eq(index).next().then((price)=>{
+                expect(price.text()).to.equal('25')
+            })
+        }
+
+    })
+
+})
+})
